cdcec8a42b727c9332b7f8643e787bd4f4607992
kittinanthanatch <kittinanthanatch.1999@gmail.com>
2025-07-30 14:44:24 +0700

    ปรับ redirect

--- shop/index.php
+++ shop/index.php
@@ -4,7 +4,11 @@ $isProtocol = isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : '
 // Set the file extension based on the protocol
 $isFile = ($isProtocol === 'http') ? '.php' : '';
 
+$uri = $_SERVER['REQUEST_URI'] ?? '/';
+$parts = explode('/', trim($uri, '/'));
+$folder = $parts[0] ?? '';
+
 // Redirect to the specified URL with the correct file extension
-header("Location: /trandar_website/store/index" . $isFile);
+header("Location: /$folder/store/index" . $isFile);
 exit();
 ?>
