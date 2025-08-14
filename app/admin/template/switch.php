72c7c5ab1296e2014e3a0213bf4ffff584f758d2
Aphisit <Apisit@origami.life>
2025-08-14 17:24:25 +0700

    switch

---
+++ app/admin/template/switch.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+$auth_url = 'https://www.origami.life';
+$app_id = 2;
+$app_token = $_SESSION['token'];
+        if($app_token){
+            $url = $auth_url."/api/oauth/v2/switch?token={$app_token}&app={$app_id}";
+            header('Location: '.$url);
+        }
+
+?>
