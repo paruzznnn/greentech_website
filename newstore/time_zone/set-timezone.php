5cd128002ca1f85eeb9f31eeec0a7cc23fcdf881
kittinanthanatch <kittinanthanatch.1999@gmail.com>
2025-08-20 23:43:59 +0700

    ปรับชื่อ folder

--- newstore/time_zone/set-timezone.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['tz'])) {
-    $_SESSION['user_timezone'] = $_POST['tz'];
-}
-?>
---
+++ oldstore/index.php
@@ -0,0 +1,16 @@
+<?php
+header("Location: app/index.php");
+exit();
+?>
+
+<?php
+
+// // Determine the request protocol
+// $isProtocol = isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http';
+// // Set the file extension based on the protocol
+// $isFile = ($isProtocol === 'http') ? '.php' : '';
+
+// // Redirect to the specified URL with the correct file extension
+// header("Location: ../newstore/index" . $isFile);
+// exit();
+?>
