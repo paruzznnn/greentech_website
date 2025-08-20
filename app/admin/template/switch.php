8c0272badfc64b193ea893fbe0911cc174aaf8de
Aphisit <Apisit@origami.life>
2025-08-20 11:53:25 +0700

    แก้ไข switch

--- app/admin/template/switch.php
+++ app/admin/template/switch.php
@@ -1,18 +1,9 @@
 <?php
     session_start();
     $auth_url = $_SESSION['redirect_url'];
-    $oid = $_SESSION['oid'];
-  
+    $oid      = $_SESSION['oid'];
     if ($oid) {
-        $dataArray = [
-            
-            'oid'  => $oid
-        ];
-        $jsonData = json_encode($dataArray);
-        echo '<form id="ssoForm" action="'.$auth_url.'" method="post">';
-        echo '<input type="hidden" name="data" value=\'' . htmlspecialchars($jsonData, ENT_QUOTES, 'UTF-8') . '\'>';
-        echo '</form>';
-        echo '<script>document.getElementById("ssoForm").submit();</script>';
+        $redirectUrl = rtrim($auth_url, '/') . '/' . urlencode($oid);
+        header("Location: $redirectUrl");
         exit();
     }
-?>
